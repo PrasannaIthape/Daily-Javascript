0ee92c4a44cdc556565230db56f16f29c36422c7
PrasannaIthape <prasannaithape7757@gmail.com>
2024-04-25 17:35:20 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -3,3 +3,15 @@ let marks = [75, 78, 65, 95, 90];
 for( let idx=0;idx<marks.length;idx++){
     console.log(idx," ",marks[idx]);
 }
+
+// For of
+for(let val of marks){
+    console.log(val);
+}
+
+// avg of marks
+let sum = 0;
+for (let i of marks) {
+  sum = sum + i;
+}
+console.log(`sum is ${sum} & Avg marks is:${sum / marks.length}`);
