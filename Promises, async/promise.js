4f56aaba8b56bdb4679e98023aeed4eabc6ba16b
PrasannaIthape <prasannaithape7757@gmail.com>
2024-05-13 13:29:54 +0530

    Chaining

--- Promises, async/promise.js
+++ Promises, async/promise.js
@@ -35,3 +35,29 @@
 // promise.catch((err)=>{
 //     console.log("error",err);
 // });
+
+// chaining
+
+function asyncFunc1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 1");
+      resolve("success");
+    }, 4000);
+  });
+}
+function asyncFunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 2");
+      resolve("success");
+    }, 4000);
+  });
+}
+console.log("Fetching Data1...");
+let p1 = asyncFunc1();
+p1.then(() => {
+  console.log("Fetching Data2...");
+  let p2 = asyncFunc2();
+  p2.then(() => {});
+});
